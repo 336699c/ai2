0c49fde0654eb2a42a5b253f2b5394212e243fd7
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 14:04:42 +0800

    Update and rename panedata.json to panedata.js

---
+++ data/panedata.js
@@ -0,0 +1,5 @@
+document.getElementById("pane-content").innerHTML=`
+<a href="/ai2/text2code"><button class="pane-button">text2code</button></a>
+<a href="/ai2/settings"><button class="pane-button">settings</button></a>
+
+`
