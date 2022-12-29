a450ec266e0f43ca325648f8f5b14549392490ed
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 14:52:41 +0800

    Update panedata.js

--- data/panedata.js
+++ data/panedata.js
@@ -1,5 +1,6 @@
 document.getElementById("pane-content").innerHTML=`
-<a href="/ai2/text2code"><button class="pane-button">text2code</button></a>
+<a href="/ai2/text2code"><button class="pane-button">Generate Code</button></a>
+<a href="/ai2/codeExplain"><button class="pane-button">Explain Code</button></a>
 <a href="/ai2/settings"><button class="pane-button">settings</button></a>
 
 `
