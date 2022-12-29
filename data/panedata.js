83ecdc7340e6e25447cb77354d5c7f6d62ff1ffc
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 16:37:38 +0800

    Update panedata.js

--- data/panedata.js
+++ data/panedata.js
@@ -1,4 +1,36 @@
 document.getElementById("pane-content").innerHTML=`
+<button class="menu-close" onclick="
+  if($('#main').hasClass('container-collaspe')){
+  $('#main').removeClass('container-collaspe')
+  $('#pane2').animate({width: '250px'}, {
+        duration: 550,
+        specialEasing: {
+            width: 'swing'
+        }
+    });
+  $('#main').animate({marginLeft: '255px'}, {
+        duration: 550,
+        specialEasing: {
+            width: 'swing'
+        }
+    });
+  $('#pane-content').slideDown(500);
+  }else{
+  $('#pane-content').slideUp(500);
+  $('#pane2').animate({width: '45px'}, {
+        duration: 550,
+        specialEasing: {
+            width: 'swing'
+        }
+    });
+  $('#main').animate({marginLeft: '50px'}, {
+        duration: 550,
+        specialEasing: {
+            width: 'swing'
+        }
+    });
+  $('#main').addClass('container-collaspe');
+  }">&#9776;</button>
 <a href="/ai2/text2code"><button class="pane-button">Generate Code</button></a>
 <a href="/ai2/codeexplain"><button class="pane-button">Explain Code</button></a>
 <a href="/ai2/settings"><button class="pane-button">Settings</button></a>
