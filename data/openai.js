1fa45be1bd8b5275d7547bb3b53e1994bc644ca0
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 18:12:33 +0800

    Update openai.js

--- data/openai.js
+++ data/openai.js
@@ -17,5 +17,22 @@ xhr.onreadystatechange = function () {
 
 xhr.send(data);
 },
-  
+dallepicture(data,apikey,callback){
+data = JSON.stringify(data);
+ 
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://api.openai.com/v1/images/generations");
+
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "Bearer "+apikey);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+       callback(xhr.responseText,xhr.status);
+   }};
+
+ xhr.send(data);
+}
 }
