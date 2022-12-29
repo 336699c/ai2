1f08b3aa4a7e6cdd1b7112bc4f44cb02c59f3334
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 16:42:50 +0800

    Update panedata.js

--- data/panedata.js
+++ data/panedata.js
@@ -1,4 +1,5 @@
-document.getElementById("pane-content").innerHTML=`
+document.getElementById("pane2").innerHTML=`
+
 <button class="menu-close" onclick="
   if($('#main').hasClass('container-collaspe')){
   $('#main').removeClass('container-collaspe')
@@ -31,8 +32,11 @@ document.getElementById("pane-content").innerHTML=`
     });
   $('#main').addClass('container-collaspe');
   }">&#9776;</button>
+
+<div class="pane-content" id="pane-content" style="display: none; top: 0px;">
 <a href="/ai2/text2code"><button class="pane-button">Generate Code</button></a>
 <a href="/ai2/codeexplain"><button class="pane-button">Explain Code</button></a>
 <a href="/ai2/settings"><button class="pane-button">Settings</button></a>
 
+</div>
 `
