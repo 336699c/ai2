d5b454f28590dd8ffb912c90c26b271f075fe4c2
336699c <63760951+336699c@users.noreply.github.com>
2022-12-29 17:13:23 +0800

    Create openai.js

---
+++ data/openai.js
@@ -0,0 +1,21 @@
+var OpenAI={
+create(data,apikey,callback){
+data = JSON.stringify(data);
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://api.openai.com/v1/completions");
+
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "Bearer "+apikey);
+
+xhr.onreadystatechange = function () {
+ if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+    callback(xhr.responseText,xhr.status);
+ }};
+
+xhr.send(data);
+},
+  
+}
